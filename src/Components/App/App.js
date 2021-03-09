af8c8b3855b71643f4a2640fd6700e599d243eac
Stacy Potten <stacylynnaz@gmail.com>
2021-03-09 16:03:13 -0700

    Import Student component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
+import Student from '../Student/Student';
 // import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <main className="main-area">
         <Header />
+        <Student />
       </main>
     );
   }
