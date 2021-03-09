fc8193c66d83b9e58dc73e30c747df73b03cfd68
Stacy Potten <stacylynnaz@gmail.com>
2021-03-09 16:24:00 -0700

    Add header to Catalog

--- src/Components/Catalog/Catalog.js
+++ src/Components/Catalog/Catalog.js
@@ -1,10 +1,12 @@
-import React from 'react'; 
+import React from 'react';
 import './Catalog.css';
 
 const Catalog = () => {
   return (
-    
-  )
-}
+    <section className="catalog">
+      <h2 className="catalog-title">Available Courses</h2>
+    </section>
+  );
+};
 
-export default Catalog; 
+export default Catalog;
