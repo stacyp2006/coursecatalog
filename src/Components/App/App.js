5f066b78a4321a195903614fb18a4944c1838e11
Stacy Potten <stacylynnaz@gmail.com>
2021-03-09 15:25:51 -0700

    Add Header component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import Header from '../Header/Header';
+// import './App.css';
 
 class App extends Component {
   render() {
     return (
       <main className="main-area">
-        <h1 className="title">Course Catalog</h1>
+        <Header />
       </main>
     );
   }
