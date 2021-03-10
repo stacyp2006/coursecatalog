2cb167e4448c3cb675c12e9e0fd1a2f37a5511a3
Stacy Potten <stacylynnaz@gmail.com>
2021-03-10 14:49:15 -0700

    Create course data file

---
+++ src/courses.js
@@ -0,0 +1,25 @@
+export const courses = [
+  {
+    name: 'English 101',
+    id: 'C123456',
+    time: '9am MWF',
+  },
+
+  {
+    name: 'History 201',
+    id: 'C234567',
+    time: '10am MWF',
+  },
+
+  {
+    name: 'College Algebra 101',
+    id: 'C345678',
+    time: '11am MWF',
+  },
+
+  {
+    name: 'Underwater Basket Weaving',
+    id: 'C456789',
+    time: '12pm MWF',
+  },
+];
