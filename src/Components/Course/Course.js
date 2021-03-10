b33a65af48b3a46c4d8f06a0959478bea4f4a55e
Stacy Potten <stacylynnaz@gmail.com>
2021-03-10 10:36:20 -0700

    Create basic course card structure

--- src/Components/Course/Course.js
+++ src/Components/Course/Course.js
@@ -1,10 +1,19 @@
-import React from 'react'; 
+import React from 'react';
 import './Course.css';
 
 const Course = () => {
   return (
-    
-  )
-}
+    <div className="course-card">
+      <section className="course-info">
+        <h3 className="course-title">Course title</h3>
+        <p className="course-id">C123456</p>
+        <p className="course-time">9am MWF</p>
+      </section>
+      <button className="register-btn" type="button">
+        Register
+      </button>
+    </div>
+  );
+};
 
 export default Course;
