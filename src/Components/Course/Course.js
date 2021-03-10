b58db7f05b142034e9e52ae1b558ea63dcc7035f
Stacy Potten <stacylynnaz@gmail.com>
2021-03-10 15:05:40 -0700

    Dynamically render course info

--- src/Components/Course/Course.js
+++ src/Components/Course/Course.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Course.css';
 
-const Course = () => {
+const Course = ({ courseInfo, addToCourseList }) => {
   return (
     <div className="course-card">
       <section className="course-info">
-        <h3 className="course-title">Course title</h3>
-        <p className="course-id">C123456</p>
-        <p className="course-time">9am MWF</p>
+        <h3 className="course-title">{courseInfo.name}</h3>
+        <p className="course-id">{courseInfo.id}</p>
+        <p className="course-time">{courseInfo.time}</p>
       </section>
-      <button className="register-btn" type="button">
+      <button
+        className="register-btn"
+        type="button"
+        value={courseInfo.id}
+        onClick={(event) => addToCourseList(event)}
+      >
         Register
       </button>
     </div>
