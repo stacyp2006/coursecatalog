871764dd12734606c93f1ec39d7ccf57eeebd1e2
Stacy Potten <stacylynnaz@gmail.com>
2021-03-10 14:53:25 -0700

    Add displayCourses method

--- src/Components/Catalog/Catalog.js
+++ src/Components/Catalog/Catalog.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import Course from '../Course/Course';
+import { courses } from '../../courses.js';
 import './Catalog.css';
 
 class Catalog extends Component {
+  displayCourses = () => {
+    return courses.map((course, i) => {
+      return (
+        <Course
+          key={i}
+          courseInfo={course}
+          addToCourseList={this.addToCourseList}
+        />
+      );
+    });
+  };
+
   render() {
+    const courseCards = this.displayCourses();
     return (
       <section className="catalog">
         <h2 className="catalog-title">Available Courses</h2>
+        <section className="course-container">{courseCards}</section>
       </section>
     );
   }
