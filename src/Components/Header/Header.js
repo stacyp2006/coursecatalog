7e4deb6833dcee7d105155630e813fa9a4354d08
Stacy Potten <stacylynnaz@gmail.com>
2021-03-11 12:37:31 -0700

    Add home link to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,9 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="page-header">
-      <h1 className="title">Course Catalog</h1>
+      <Link className="homelink" to="/">
+        <h1 className="title">Course Catalog</h1>
+      </Link>
       <nav className="nav-area">
         <Link className="my-courses-link" to="/mycourses">
           My Courses
