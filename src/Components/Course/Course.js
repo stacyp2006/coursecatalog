1493fbe13728399aa5e40d464a0a93c18d1a274b
Stacy Potten <stacylynnaz@gmail.com>
2021-03-11 10:20:00 -0700

    Add methods for course registration

--- src/Components/Course/Course.js
+++ src/Components/Course/Course.js
@@ -1,24 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Course.css';
 
-const Course = ({ courseInfo, addToCourseList }) => {
-  return (
-    <div className="course-card">
-      <section className="course-info">
-        <h3 className="course-title">{courseInfo.name}</h3>
-        <p className="course-id">{courseInfo.id}</p>
-        <p className="course-time">{courseInfo.time}</p>
-      </section>
-      <button
-        className="register-btn"
-        type="button"
-        value={courseInfo.id}
-        onClick={(event) => addToCourseList(event)}
-      >
-        Register
-      </button>
-    </div>
-  );
-};
+class Course extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMyCourse: false,
+    };
+  }
+
+  addCourse = (event) => {
+    let newCourse = {
+      name: this.props.name,
+      key: this.props.id,
+      id: this.props.id,
+      time: this.props.time,
+    };
+    this.setState({ isMyCourse: true });
+    this.props.addToMyCourses(newCourse);
+  };
+
+  removeCourse = (event) => {
+    this.setState({ isMyCourse: false });
+    let id = this.props.courseInfo.id;
+    this.props.removeFromMyCourses(id);
+  };
+
+  render() {
+    return (
+      <div className="course-card">
+        <section className="course-info">
+          <h3 className="course-title">{this.props.courseInfo.name}</h3>
+          <p className="course-id">{this.props.courseInfo.id}</p>
+          <p className="course-time">{this.props.courseInfo.time}</p>
+        </section>
+        {!this.state.isMyCourse && (
+          <button
+            className="register-btn"
+            type="button"
+            value={this.props.courseInfo.id}
+            onClick={(event) => this.addCourse(event)}
+          >
+            Register
+          </button>
+        )}
+        {this.state.isMyCourse && (
+          <button
+            className="remomve-btn"
+            type="button"
+            value={this.props.courseInfo.id}
+            onClick={(event) => this.removeCourse(event)}
+          >
+            Remove
+          </button>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Course;
